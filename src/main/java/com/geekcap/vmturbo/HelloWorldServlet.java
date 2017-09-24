de1d24b0cbb131c324174e5e76a1a5ed3e75a5e4
cskanukuntla <cskanukuntla@yahoo.com>
2017-09-24 16:51:55 -0400

    added message 2

--- src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
+++ src/main/java/com/geekcap/vmturbo/HelloWorldServlet.java
@@ -8,7 +8,7 @@ public class HelloWorldServlet extends HttpServlet
 {
   public void service( HttpServletRequest req, HttpServletResponse res ) throws IOException {
     PrintWriter out = res.getWriter();
-    out.println( "asdfasd asdfasd Hello, World!" );
+    out.println( " Hello, World!" );
     out.close();
   }
 }
